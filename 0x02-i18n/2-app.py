1416b7280461b8c4183cb1995a7c1abc469713f7
KiproticKibor <kiborotich24@gmail.com>
2024-09-25 00:42:37 +0300

    syntax changes

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,6 +20,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """
     Determine the best match with our supported languages
@@ -27,15 +28,12 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route('/', strict_slashes=False)
 def index():
     """
     Render the index page
     """
-    return render_template('2-index.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
